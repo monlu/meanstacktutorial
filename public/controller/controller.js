9e02bc94695cc49affaebc05a400add2c51aad3d
George Lu <georgemenglu@gmail.com>
2015-08-06 08:52:10 -0400

    5/5 18 min

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,14 +1,13 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppController', ['$scope', '$http', function ($scope, $http) {
-	console.log("Hello World from controller")
 
 	var refresh = function () {
 		$http.get('/contactlist').success(function (res) {
 			console.log("i got the data")
 			$scope.contactlist = res;
 			$scope.contact + "";
-		})
-	}
+		});
+	};
 
 	refresh();
 
@@ -18,6 +17,26 @@ myApp.controller('AppController', ['$scope', '$http', function ($scope, $http) {
 			console.log(res);
 			refresh();
 		});
+	};
+
+	$scope.remove = function (id) {
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function (res) {
+			console.log(res);
+			refresh();
+		});
+	};
+
+	$scope.edit = function (id) {
+		console.log(id);
+		$http.get('/contactlist/' + id).success(function (res) {
+			$scope.contact = res;
+		})
+	}
+
+	$scope.update = function () {
+		console.log($scope.contact._id);
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact)
 	}
 
 }])
--- server.js
+++ server.js
@@ -23,6 +23,33 @@ app.post('/contactlist', function (req, res) {
 	});
 });
 
+app.delete('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({ _id: mongojs.ObjectId(id) }, function (err, docs) {
+		res.json(docs);
+	});
+});
+
+app.get('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({ _id: mongojs.ObjectId(id) }, function (err, docs) {
+		res.json(docs);
+	});
+});
+
+app.put('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({ query: {_id: mongojs.ObjectId(id)},
+		update: { $set: {name: req.body.name, email: req.body.email,  number: req.body.number },
+		new: true }, function (err, docs) {
+			res.json(docs);
+		}
+	});
+});
+
 app.listen(3000, function () {
 	console.log("localhost:3000")
 });
